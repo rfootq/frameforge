57b908c129ad97a21632080bfb631c9d8610eae3
Vivien Henry <vivien.henry@inductivebrain.fr>
2024-10-06 20:14:10 +0200

    ui

--- freecad/frameforge/create_profiles_tool.py
+++ freecad/frameforge/create_profiles_tool.py
@@ -37,6 +37,9 @@ class CreateProfileTaskPanel():
         self.form.combo_family.currentIndexChanged.connect(self.on_family_changed)
         self.form.combo_size.currentIndexChanged.connect(self.on_size_changed)
 
+        self.form.cb_make_fillet.stateChanged.connect(self.on_cb_make_fillet_changed)
+
+
         self.form.combo_material.addItems([k for k in self.profiles])
 
 
@@ -47,10 +50,34 @@ class CreateProfileTaskPanel():
         self.form.combo_family.addItems([f for f in self.profiles[material]])
 
     def on_family_changed(self, index):
-        pass
+        material = str(self.form.combo_material.currentText())
+        family = str(self.form.combo_family.currentText())
+
+        self.form.cb_make_fillet.setChecked(self.profiles[material][family]['fillet'])
+        self.form.cb_make_fillet.setEnabled(self.profiles[material][family]['fillet'])
+
+        self.update_image()
+        
 
     def on_size_changed(self, index):
         pass
+
+
+    def on_cb_make_fillet_changed(self, state):
+        self.update_image()
+
+
+
+    def update_image(self):
+        family = str(self.form.combo_family.currentText())
+
+        img_name = family.replace(' ', "_")
+        if self.form.cb_make_fillet.isChecked():
+            img_name += "_Fillet"
+        img_name += ".png"
+        App.Console.PrintMessage(translate("frameforge", img_name + "\n"))
+        self.form.label_image.setPixmap(QtGui.QPixmap(os.path.join(PROFILEIMAGES_PATH, img_name)))
+
 
     def open(self):
         App.Console.PrintMessage(translate("frameforge", "Opening CreateProfile\n"))
