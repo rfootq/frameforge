d113fe38c0e63df33bf08f7c91a15068c49c2a49
Vivien Henry <vivien.henry@inductivebrain.fr>
2024-10-12 13:33:27 +0200

    add commands

--- freecad/frameforge/init_gui.py
+++ freecad/frameforge/init_gui.py
@@ -13,7 +13,23 @@ class FrameForge(Gui.Workbench):
     MenuText = translate("frameforge", "FrameForge")
     ToolTip = translate("frameforge", "a simple FrameForge")
     Icon = os.path.join(ICONPATH, "metalwb.svg")
-    toolbox = ["FrameForge_CreateProfiles"]
+
+    toolbox_drawing = [
+        "Sketcher_NewSketch",
+
+    ]
+
+    toolbox_frameforge = [
+        "FrameForge_CreateProfiles",
+        "FrameForge_TrimProfiles",
+        "FrameForge_EndMiter"
+    ]
+
+    toolbox_part = [
+        "Part_Fuse",
+        "Part_Cut",
+
+    ]
 
     def GetClassName(self):
         return "Gui::PythonWorkbench"
@@ -23,7 +39,7 @@ class FrameForge(Gui.Workbench):
         This function is called at the first activation of the workbench.
         here is the place to import all the commands
         """
-        from freecad.frameforge import create_profiles_tool
+        from freecad.frameforge import create_profiles_tool, create_trimmed_profiles_tool, create_end_miter_tool
 
         # Add translations path
         Gui.addLanguagePath(TRANSLATIONSPATH)
@@ -33,8 +49,14 @@ class FrameForge(Gui.Workbench):
             "frameforge",
             "Switching to frameforge") + "\n")
 
-        self.appendToolbar(translate("Toolbar", "Tools"), self.toolbox)
-        self.appendMenu(translate("Menu", "Tools"), self.toolbox)
+        self.appendToolbar(translate("frameforge", "Drawing Primitives"), self.toolbox_drawing)
+        self.appendMenu(translate("frameforge", "Drawing Primitives"), self.toolbox_drawing)
+
+        self.appendToolbar(translate("frameforge", "Frameforge"), self.toolbox_frameforge)
+        self.appendMenu(translate("frameforge", "Frameforge"), self.toolbox_frameforge)
+
+        self.appendToolbar(translate("frameforge", "Part Primitives"), self.toolbox_part)
+        self.appendMenu(translate("frameforge", "Part Primitives"), self.toolbox_part)
 
     def Activated(self):
         '''
