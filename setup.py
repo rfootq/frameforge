3e94d0c21a7dd4c215c123f39362ec8053215c76
Vivien Henry <vivien.henry@inductivebrain.fr>
2024-10-13 10:52:41 +0200

    description

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setup(name='frameforge',
       maintainer="Vivien HENRY",
       maintainer_email="vivien.henry@inductivebrain.fr",
       url="https://github.com/lukh/frameforge",
-      description="Frameforge helps designing beams and frames, with cut, mitter joins and so on",
+      description="FrameForge is dedicated for creating Frames and Beams, and apply operations (miter cuts, trim cuts) on theses profiles.",
       include_package_data=True)
